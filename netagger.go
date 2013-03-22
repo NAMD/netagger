84909034e0e56377ca225853e2ae6b115132199f
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-03-22 10:10:28 -0300

    Reads lines from a file

---
+++ netagger.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "bufio"
+    "io"
+    "fmt"
+    "log"
+    "os"
+)
+
+func load_data(path string) map[string]string {
+    data := make(map[string]string)
+    fp, err := os.Open(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    bf := bufio.NewReader(fp)
+    for {
+        line, isPrefix, err := bf.ReadLine()
+
+        if err == io.EOF {
+            break
+        }
+
+        if isPrefix {
+            log.Fatal("Line too long")
+        }
+
+        word := string(line)
+        data[word] = word
+    }
+    return data
+}
+
+func main() {
+    countries := load_data("KnownLists/en/known_country.lst")
+    fmt.Println(countries["Brasil"])
+}
